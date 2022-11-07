2466f296d8f4534f394cd8054cd8b819f7938d77
anandpsan <anand130188@gmail.com>
2022-11-07 20:22:16 +0530

    New Maven Project

---
+++ src/test/java/org/test/Sample.java
@@ -0,0 +1,20 @@
+package org.test;
+
+public class Sample {
+	
+	public void test1() {
+		System.out.println("1");
+
+	}
+	
+	public void test2() {
+		System.out.println("2");
+
+	}
+	
+	public void test3() {
+		System.out.println("3");
+
+	}
+
+}
